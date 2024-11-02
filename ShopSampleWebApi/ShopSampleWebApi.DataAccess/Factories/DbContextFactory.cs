f6632e917bc2efd282c08330a948471afb601eb7
Martin Penkava <martin.pinky@seznam.cz>
2024-11-02 12:13:40 +0100

    test: Create DataAccess.Tests project for unit testing the DataAccess layer
    - Created DataAccess.Tests class library as a dedicated project for unit tests of the DataAccess layer.
    - Added ProductRepositoryTests with basic unit tests for Product entity operations.
    - Implemented DbContextFactory to provide DbContext instances for testing purposes.

---
+++ ShopSampleWebApi/ShopSampleWebApi.DataAccess/Factories/DbContextFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShopSampleWebApi.DataAccess.Factories
+{
+    /// <summary>
+    /// Enum representing the type of database to be used.
+    /// </summary>
+    public enum DatabaseType
+    {
+        InMemory,
+        Real
+    }
+
+    /// <summary>
+    /// Factory class for creating instances of ApplicationDbContext.
+    /// </summary>
+    public static class DbContextFactory
+    {
+        /// <summary>
+        /// Creates an instance of ApplicationDbContext based on the specified database type.
+        /// </summary>
+        /// <param name="type">The type of database to be used.</param>
+        /// <param name="connectionString">The connection string for the real database. Optional for InMemory database.</param>
+        /// <returns>An instance of ApplicationDbContext.</returns>
+        public static ApplicationDbContext CreateDbContext(DatabaseType type, string? connectionString = null)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+
+            if (type == DatabaseType.InMemory)
+                optionsBuilder.UseInMemoryDatabase("TestDatabase");
+            else if (type == DatabaseType.Real && connectionString != null)
+                optionsBuilder.UseSqlServer(connectionString);
+
+            var context = new ApplicationDbContext(optionsBuilder.Options);
+
+            // Ensure the database is deleted and created anew for each test when using InMemory database.
+            if (type == DatabaseType.InMemory)
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+
+            return context;
+        }
+    }
+}
---
+++ ShopSampleWebApi/Tests/ShopSampleWebApi.DataAccess.Tests/ProductRepositoryTests.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopSampleWebApi.DataAccess.Factories;
+using ShopSampleWebApi.DataAccess.Models;
+using ShopSampleWebApi.DataAccess.Repositories;
+using ShopSampleWebApi.Helpers;
+using Xunit;
+
+namespace ShopSampleWebApi.DataAccess.Tests
+{
+    public class ProductRepositoryTests : IAsyncLifetime
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly ProductRepository _productRepository;
+
+        public ProductRepositoryTests()
+        {
+            var databaseSettings = ConfigurationHelper.GetDatabaseSettings();
+
+            // Set up the database based on the value from DatabaseSettings configuration.
+            _context = DbContextFactory.CreateDbContext(
+                databaseSettings.DatabaseType == "InMemory" ? DatabaseType.InMemory : DatabaseType.Real,
+                databaseSettings.RealDatabaseConnectionString
+            );
+            _context = DbContextFactory.CreateDbContext(DatabaseType.InMemory);
+            _productRepository = new ProductRepository(_context);
+        }
+
+        public Task InitializeAsync()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task DisposeAsync()
+        {
+            // Delete the database after each test (optional for InMemory).
+            if (_context.Database.IsInMemory())
+                _context.Database.EnsureDeleted();
+
+            _context.Dispose();
+            return Task.CompletedTask;
+        }
+
+        [Fact]
+        public async Task AddAsync_ShouldAddProduct()
+        {
+            var product = new Product { Name = "TestProduct", ImgUri = "https://picsum.photos/g/200/200/?image=123", Price = 1000 };
+
+            await _productRepository.AddAsync(product);
+
+            var addedProduct = await _context.Products.FindAsync(product.Id);
+
+            // Assert that the added product is not null.
+            Assert.NotNull(addedProduct);
+            // Assert that the name of the added product is "TestProduct".
+            Assert.Equal("TestProduct", addedProduct.Name);
+            // Assert that the image URI of the added product is not null.
+            Assert.NotNull(addedProduct.ImgUri);
+            // Assert that the price of the added product is 1000.
+            Assert.Equal(1000, addedProduct.Price);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ShouldReturnAllProducts()
+        {
+            var products = await _productRepository.GetAllAsync();
+
+            // Assert that the products count initialization is 100.
+            Assert.Equal(100, ((List<Product>)products).Count);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_WithPaging_ShouldReturnPagedResults()
+        {
+            var pagedProducts = await _productRepository.GetAllAsync(1, 1); // Page 1, page size 1.
+
+            // Assert that the paged result contains a single item.
+            Assert.Single(pagedProducts.Items);
+            // Assert that the total count of products is 100.
+            Assert.Equal(100, pagedProducts.TotalCount);
+        }
+    }
+}
